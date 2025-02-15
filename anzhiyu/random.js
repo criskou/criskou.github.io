db50754032a488fa960432584b89e1370adb5917
criskou <3163186039@qq.com>
2025-02-15 10:03:46 +0800

    Site updated: 2025-02-15 10:03:46

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/14/Fiddler-工具/","2025/02/12/Python语言/","2025/02/14/Postman-工具/"];function toRandomPost(){
+var posts=["2025/02/14/Fiddler-工具/","2025/02/12/Python语言/","2025/02/14/Postman-工具/","2025/02/15/测试理论/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
