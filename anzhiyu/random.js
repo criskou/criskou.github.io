99c232d722e95cf92846cba3744fb3a4f38e3563
criskou <3163186039@qq.com>
2025-02-16 21:25:50 +0800

    Site updated: 2025-02-16 21:25:50

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/14/Fiddler-工具/","2025/02/14/Postman-工具/","2025/02/12/Python语言/","2025/02/15/测试理论/","2025/02/15/JMeter-工具/"];function toRandomPost(){
+var posts=["2025/02/14/Fiddler-工具/","2025/02/14/Postman-工具/","2025/02/12/Python语言/","2025/02/15/测试理论/","2025/02/15/JMeter-工具/","2025/02/16/读书笔记/","2025/02/16/自动化测试/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
